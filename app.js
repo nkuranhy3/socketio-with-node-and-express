caf28e4f4ba314ff19ccd07be38febbafdbd8722
Alfred Boateng <programmingwithalfred@gmail.com>
2018-04-16 15:07:30 +0000

    app file updated

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ app.use(express.static(path.join(__dirname, "public")));
 
 var socket = require("socket.io");
 
-
-var server = app.listen(3000, function(){
-    console.log("server has started on port 3000");
+var server = app.listen(3000, function () {
+  console.log("server has started on port 3000");
 });
 
 
-/*  socket set up */
+/* Step 1, set up socket */
+
 var io = socket(server);
 
-io.on("connection", function (socket){
-    console.log("A new connection made", socket.id);
+io.on("connection", function (socket) {
+  console.log("There is a New Connection with  id: ", socket.id);
 
-    /* step 2 . servaer handles event from front end */
+  /* step 3 . server handles event from client */
 
-    socket.on("chat", function(data){
-        io.sockets.emit("chat", data);
-    });
-    /* handle broadcast from front-end */
+  socket.on("chat", function (data) {
+    io.sockets.emit("chat", data);
+  });
+  
+  /* step 6. handle broadcast from client*/
 
-    socket.on("typing", function (data) {
-        socket.broadcast.emit("typing", data);
-    });
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
-
--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,7 @@
 /* global io */
 var socket = io.connect("http://localhost:3000/");
 
+
 var message = document.getElementById("message"),
     handle = document.getElementById("handle"), 
     btn = document.getElementById("send"),
@@ -8,8 +9,7 @@ var message = document.getElementById("message"),
     feedback = document.getElementById("feedback");
 
 
-/*  step 1 . emit event  and send to server*/
-
+/*  step 2 . emit event  and send to server*/
 
 btn.addEventListener("click", function(){
     socket.emit("chat", {
@@ -18,18 +18,22 @@ btn.addEventListener("click", function(){
     });
 });
 
-/* adding broadcast feature */
-message.addEventListener("keypress", function(){
-   
-    socket.emit("typing", handle.value);
-});
 
-/* step 3 . listen for events from server */
+
+/* step 4 . listen for events from server */
 
 socket.on("chat", function(data){
     feedback.innerHTML ="";
     output.innerHTML += "<p><strong>" + data.handle + "</strong> : " + data.message +"</p>";
 });
+
+/* Step 5. adding broadcast feature */
+
+message.addEventListener("keypress", function(){
+    socket.emit("typing", handle.value);
+});
+
+/*displaying broadcast message from server*/
 
 socket.on("typing", function(data){
     feedback.innerHTML="<p><em>"+ data + "  is typing..." + "</em></p>";
