6e1e7b0c37c55f6d5281e31348b76c4dc7d2b1d0
Alfred Boateng <programmingwithalfred@gmail.com>
2018-04-19 23:57:46 +0000

    disconnect notification

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 var socket = require("socket.io");
 
-var server = app.listen(3000, function () {
-  console.log("server has started on port 3000");
+var server = app.listen(process.env.PORT, process.env.IP || 3000, function () {
+  console.log("server has started on port");
+});
+
+app.get("/", function(req, res){
+  res.sendFile("index.html");
 });
 
 
@@ -15,17 +19,21 @@ var server = app.listen(3000, function () {
 var io = socket(server);
 
 io.on("connection", function (socket) {
-  console.log("There is a New Connection with  id: ", socket.id);
-
-  /* step 3 . server handles event from client */
-
-  socket.on("chat", function (data) {
-    io.sockets.emit("chat", data);
-  });
-  
-  /* step 6. handle broadcast from client*/
-
-  socket.on("typing", function (data) {
-    socket.broadcast.emit("typing", data);
-  });
+      console.log("There is a New Connection with  id: ", socket.id);
+      
+      socket.on("disconnect", function(){
+        console.log("A user disconnected with id " + socket.id);
+      });
+    
+      /* step 3 . server handles event from client */
+    
+      socket.on("chat", function (data) {
+        io.sockets.emit("chat", data);
+      });
+      
+      /* step 6. handle broadcast from client*/
+    
+      socket.on("typing", function (data) {
+        socket.broadcast.emit("typing", data);
+      });
 });
